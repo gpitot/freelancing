16df7552c8fee7eb262e773d250cb52588bc1d45
Guillaume Pitot <guillaume.pitot@gmail.com>
2019-01-05 21:22:36 +1100

    Updates

---
+++ precache-manifest.316dd57bf13e1af30554a89e007cd7cf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "c94789f7f8415e530c5cb0ac5462d46f",
+    "url": "/freelancing/static/media/beach3.c94789f7.jpeg"
+  },
+  {
+    "revision": "01b9d8ff8b1395544ea9",
+    "url": "/freelancing/static/css/main.68a5a0d5.chunk.css"
+  },
+  {
+    "revision": "959cb1912bacd1e2dde3",
+    "url": "/freelancing/static/js/1.959cb191.chunk.js"
+  },
+  {
+    "revision": "b6597663644752f98ad7",
+    "url": "/freelancing/static/js/runtime~main.b6597663.js"
+  },
+  {
+    "revision": "fe90f29bfa62ed3e2fd6815e0230a955",
+    "url": "/freelancing/static/media/logo3.fe90f29b.png"
+  },
+  {
+    "revision": "81020a2a0fcd79b3cd537a9cda0aca63",
+    "url": "/freelancing/static/media/beach_vertical.81020a2a.jpeg"
+  },
+  {
+    "revision": "01b9d8ff8b1395544ea9",
+    "url": "/freelancing/static/js/main.01b9d8ff.chunk.js"
+  },
+  {
+    "revision": "5aec8b09b6944258939259b33d67fddd",
+    "url": "/freelancing/static/media/beach4.5aec8b09.jpeg"
+  },
+  {
+    "revision": "866d12633d8da557b2fdf6c8a7eacadb",
+    "url": "/freelancing/static/media/sand.866d1263.jpeg"
+  },
+  {
+    "revision": "d93122218ad8026b19724174d8e8e2b3",
+    "url": "/freelancing/static/media/dunes_v.d9312221.jpeg"
+  },
+  {
+    "revision": "482930d41c713d0c87f658a1c2c5d674",
+    "url": "/freelancing/static/media/sand_vertical.482930d4.jpeg"
+  },
+  {
+    "revision": "51ad466db13b3f1290cd4aa5b46d541e",
+    "url": "/freelancing/static/media/discount.51ad466d.png"
+  },
+  {
+    "revision": "df03ae9c01c85eb2119f87e9a4bf814a",
+    "url": "/freelancing/index.html"
+  }
+];
