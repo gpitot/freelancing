0d4d0322b7589351adb7ac87ba93ea9b889215d8
Guillaume Pitot <guillaume.pitot@gmail.com>
2019-02-20 20:00:39 +1100

    Updates

---
+++ precache-manifest.d0fda233eba1cfefd89bf01b86d7648b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "5aec8b09b6944258939259b33d67fddd",
+    "url": "/static/media/beach4.5aec8b09.jpeg"
+  },
+  {
+    "revision": "2eb19277c31fcf8013ae",
+    "url": "/static/css/main.0035d60c.chunk.css"
+  },
+  {
+    "revision": "db2590ee5533ea0301c7",
+    "url": "/static/js/1.db2590ee.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e094724c090350c59bc2e2f1c6c7453d",
+    "url": "/static/media/logo4.e094724c.png"
+  },
+  {
+    "revision": "81020a2a0fcd79b3cd537a9cda0aca63",
+    "url": "/static/media/beach_vertical.81020a2a.jpeg"
+  },
+  {
+    "revision": "b83df7cc3aa96eab8ad70f716db717df",
+    "url": "/static/media/beach.b83df7cc.jpg"
+  },
+  {
+    "revision": "2eb19277c31fcf8013ae",
+    "url": "/static/js/main.2eb19277.chunk.js"
+  },
+  {
+    "revision": "866d12633d8da557b2fdf6c8a7eacadb",
+    "url": "/static/media/sand.866d1263.jpeg"
+  },
+  {
+    "revision": "d93122218ad8026b19724174d8e8e2b3",
+    "url": "/static/media/dunes_v.d9312221.jpeg"
+  },
+  {
+    "revision": "482930d41c713d0c87f658a1c2c5d674",
+    "url": "/static/media/sand_vertical.482930d4.jpeg"
+  },
+  {
+    "revision": "51ad466db13b3f1290cd4aa5b46d541e",
+    "url": "/static/media/discount.51ad466d.png"
+  },
+  {
+    "revision": "933b1c1d68e0e3588b6009fd739512a7",
+    "url": "/static/media/drawingroom.933b1c1d.webm"
+  },
+  {
+    "revision": "ef661b4c5826672c63dac7edb3edc9cc",
+    "url": "/static/media/notonmyroad.ef661b4c.mp4"
+  },
+  {
+    "revision": "42458c88fa29bdb97a3f1d285de263a4",
+    "url": "/index.html"
+  }
+];
--- src/pages/home/components/Loading.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Loading = (props) => {
-    return null;
-}
-
-export default Loading;
